265bbdc6d95c3880b832a520b7a6a0d8a1966afb
Kevin Brandt <kbrandt@plos.org>
2018-11-08 15:48:59 -0800

    first commit with structure of project

---
+++ kpbrandt/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+
+from . import views
+
+urlpatterns = [path('confucius', views.confucius, name='confucius'),
+               path('bs', views.bs, name='bs')
+               ]
---
+++ kpbrandt/portfolio/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.contrib import messages
+from django.http import HttpResponse
+
+def index(request):
+  #return render(request, 'index.html')
+  return HttpResponse("Hello, world.")
